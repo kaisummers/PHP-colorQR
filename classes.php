ed768ec584b71cc3bf9a22ec64fa51a94c9e9bce
Kai Summers <ksdigitalmedia@gmail.com>
2018-03-14 19:25:31 +0800

    Update classes.php

--- classes.php
+++ classes.php
@@ -15,22 +15,21 @@ class colorQR
             		$n = $n == 7 ? 0 : ++$n;
             		$a[$m[$k]] = $p[floor(($k)/8)]." ".$p[$n];
         	}
-       		$GLOBALS['matrix'] = $a;
+       		$GLOBALS['m'] = $a;
+		$GLOBALS['w'] = 5;
+		$GLOBALS['c'] = "<style>.cqr_cont{border:3px dashed black}.cqr{display:inline-block;width:".$GLOBALS['w']."px;height:".$GLOBALS['w']."px;}.w{background-color:white}.b{background-color:black}.r{background-color:red}.o{background-color:orange}.y{background-color:yellow}.g{background-color:limegreen}.c{background-color:blue}.m{background-color:pink}</style>";
     	}
     
 	// Create colorQR                    
 	public static function create($d)
-	{
-		// Set width & height
-		$w = 5;
-		
+	{	
         	// Encode as base62
 		$c = new base62();
 		$d = $c->encode($d);
 		
 		// Calculate Row width
         	$r = ceil(sqrt(strlen($d)*2));
-		$f = $w*$r."px";
+		$f = $GLOBALS['w']*$r."px";
         
 		// ColorQR Container
 		$q = "<div class=\"cqr_cont\" style=\"width:$f!important;height:$f!important;\">";
@@ -38,12 +37,12 @@ class colorQR
 		// Make colorQR elements
 		foreach(str_split($d) as $v)
 		{
-                	$e = explode(" ", $GLOBALS['matrix'][$v]);
+                	$e = explode(" ", $GLOBALS['m'][$v]);
                 	$q .= "<span class=\"cqr ".$e[0]."\"></span><span class=\"cqr ".$e[1]."\"></span>";
         	}
         	$q .= "</div>";
 		
-		return $q;
+		return $GLOBALS['c'].$q;
 	}	
 }
  
